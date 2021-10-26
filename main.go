e915c77883795a7d42d23f0341cfd74f95b74c64
Nicolas Gomez Alvarez <nicolas2029@hotmail.com>
2021-10-26 13:38:45 -0500

    return a json object

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hola Mundo")
+func holaMundo(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Hola Mundo"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+	w.Write(jsonResp)
 }
 
 func main() {
-	http.HandleFunc("/", home)
+	http.HandleFunc("/api/v1/hola", holaMundo)
+	fmt.Println("Server iniciado en el puerto 3000")
 	http.ListenAndServe(":3000", nil)
 }
