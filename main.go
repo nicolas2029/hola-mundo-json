b2d27fcce410ec19980099b85eb948580caa7fe0
Nicolas Gomez Alvarez <nicolas2029@hotmail.com>
2021-10-26 13:28:05 -0500

    Hola Mundo

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hola Mundo")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":3000", nil)
+}
